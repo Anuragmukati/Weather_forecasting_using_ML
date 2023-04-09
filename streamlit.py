712f187033160cf7c933f30cc4a1a76030d0da55
Anurag Mukati <anuragmukati09@gmail.com>
2023-04-09 19:05:25 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-from streamlit_pandas_profiling import st_profile_report
 from pandas_profiling import ProfileReport
 
 df = pd.read_csv('bengaluru.csv', parse_dates=['date_time'], index_col='date_time')
